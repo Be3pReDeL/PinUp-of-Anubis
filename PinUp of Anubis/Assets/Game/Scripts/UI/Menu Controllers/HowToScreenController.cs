a2539da7cfd9cd24ebed61fcf5a39b3f1c77644b
Be3pReDeL <bogatovsergey111@gmail.com>
2024-01-09 21:42:25 +0300

    Main Menu UI Done

---
+++ PinUp of Anubis/Assets/Game/Scripts/UI/Menu Controllers/HowToScreenController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class HowToScreenController : UIController {
+    [SerializeField] private float _duration = 1f;
+
+    [OPS.Obfuscator.Attribute.DoNotRename]
+    public void OnEnable() {
+        for (int i = 0; i < _tweenObjects.Count; i++)
+            _tweenObjects[i].Appear(_duration);
+    }
+
+    private void Start(){
+        for (int i = 0; i < _tweenObjects.Count; i++)
+            _tweenObjects[i].Appear(_duration);
+    }
+
+    [OPS.Obfuscator.Attribute.DoNotRename]
+    public void Close(){
+        for (int i = 0; i < _tweenObjects.Count; i++)
+            _tweenObjects[i].Disappear(_duration);
+    }
+}
